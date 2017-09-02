57596b275e0bd57d6491a057bd206b4ff38d1585
pjreiniger <pj.reiniger@gmail.com>
2017-09-02 14:23:08 -0400

    Trying deploy again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 
+echo "Starting script...."
 git clone --depth=50 --branch=master https://github.com/pjreiniger/maven_repo.git ../../pjreiniger/maven_repo
+echo "Cloned repo"
 ./gradlew build publish -Pmaven_repo=../../pjreiniger/maven_repo
+echo "Running with publish"
 cd ../../pjreiniger/maven_repo
+echo "CD"
+ls
 ./commit_updates.sh
