f165d384ab0d01285cfade3cb2abbe9c9776cd96
pjreiniger <pj.reiniger@gmail.com>
2017-09-02 14:43:13 -0400

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
 
 echo "Starting script...."
+pwd
+ls
 git clone --depth=50 --branch=master https://github.com/pjreiniger/maven_repo.git ../../pjreiniger/maven_repo
 echo "Cloned repo"
 ./gradlew build publish -Pmaven_repo=../../pjreiniger/maven_repo
-echo "Running with publish"
+echo "Ran publish"
+pwd
+ls 
+ls ..
+ls ../..
 cd ../../pjreiniger/maven_repo
 echo "CD"
 ls
