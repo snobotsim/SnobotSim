1d72f52c52053dcccecc4cb73251b081a50f4554
PJ Reiniger <pj.reiniger@gmail.com>
2020-11-13 21:38:52 -0500

    Creating a dummy project to make dependabot easier (#200)

---
+++ dependabot_helper/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public final class Main
+{
+    private Main()
+    {
+        
+    }
+
+    public static void main(String[] aArgs)
+    {
+        System.out.println("Hello world"); // NOPMD
+    }
+}
